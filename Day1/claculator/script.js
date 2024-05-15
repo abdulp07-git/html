0b27f75c043fb1c77a334c44deb4a62fc9c10076
Abdul.Jabbar <abdul@intodepth.in>
2024-05-15 10:13:59 +0530

    javascript-calculator

---
+++ Day1/claculator/script.js
@@ -0,0 +1,20 @@
+function calculate(val)
+{
+    document.getElementById('output').value = document.getElementById('output').value + val;
+}
+
+
+
+function cleartext()
+{
+    document.getElementById('output').value = "";
+}
+
+
+function EqualResult()
+{
+    var text=document.getElementById('output').value;
+    var result=eval(text);
+    console.log(result);
+    document.getElementById('output').value=result;
+}
